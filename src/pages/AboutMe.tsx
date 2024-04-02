0e56119fd828c322cf2e0752905185013cd2079f
Filip Skaug <filis@stud.ntnu.no>
2024-04-02 23:09:26 +0200

    update information

--- src/pages/AboutMe.tsx
+++ src/pages/AboutMe.tsx
@@ -11,9 +11,8 @@ function AboutMe() {
           <div className="textbox">
             <h1>Om meg</h1>
             <p>
-              Jeg heter Filip og studerer for øyeblikket 3-året på Bachelor i informatikk ved NTNU. 
-              Jeg er fornøyd med studiet å planlegger derfor å ta Master i informatikk, 
-              og ser fram til å arbeide innenfor IT-sektoren.
+              Jeg heter Filip Skaug og studerer for øyeblikket 3.-året på Bachelor Informatikk ved NTNU. Jeg er fornøyd med 
+              studiet og planlegger derfor å ta Master i Informatikk og ser fram til å arbeide innenfor IT-sektoren.
             </p>
           </div>
           <img className="imageBox" id="profilbilde2" src={profilbilde2}></img>
@@ -22,14 +21,12 @@ function AboutMe() {
           <div className="textbox">
             <h1>Stien til Informatikk</h1>
             <p>
-              Jeg har alltid vært interessert i hvordan ting fungerer. Og jeg startet derfor
-              på en videregående skole med fordypning i både Realfag, Data og Elektronikk. Hvor jeg blant annet
-              ble jeg introdusert til webutvikling og mikrokontrollere som Arduino og Raspberry Pi, dette videre har utviklet
-              seg til en viktig hobby.
+              Jeg har alltid vært interessert i hvordan ting fungerer., og startet derfor på en videregående skole med fordypning i 
+              både Realfag, Data og Elektronikk. Her ble jeg blant annet introdusert for webutvikling og mikrokontrollere som 
+              Arduino og Raspberry Pi, noe som senere har utviklet seg til en viktig hobby.
               <br></br>
-              Jeg synes at programmering delen av mikrokontrollerene var mye mer utfordrende og givende, og bestemte meg
-              for å lete etter studier som var rettet mot programmering. Hvor jeg her har videre utviklet interesse innenfor
-              webutvikling og UX-design.
+              Jeg synes at programmeringsdelen av mikrokontrollerene var mye mer utfordrende og givende, og bestemte meg for å lete etter 
+              studier rettet mot programmering. På NTNU har jeg videreutviklet interesse innenfor webutvikling og UX-design.
             </p>
           </div>
           <img className="imageBox" id="sti" src={sti}></img>
@@ -39,11 +36,11 @@ function AboutMe() {
           <div className="textbox">
             <h1>Andre interesser</h1>
             <p>
-              Jeg er utrolig glad i friluft å natur, så når du ikke finner meg foran en skjerm
-              er jeg mest sansynlig på fjellet, enten i en hytte eller i en hengekøye.
+              Jeg er utrolig glad i friluft og natur, så når du ikke finner meg foran en skjerm
+              er jeg mest sannsynlig på fjellet, enten i en hytte eller i en hengekøye.
             </p>
             <p>
-              Jeg er også glad i å prøve ut nye utfordringer, så om det er buldring, curlig eller andre nye 
+              Jeg er også glad i og prøve ut nye utfordringer, så om det er buldring, curlig eller andre nye 
               ting er jeg alltid med.
             </p>
           </div>
