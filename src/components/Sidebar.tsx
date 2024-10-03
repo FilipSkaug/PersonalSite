9e01c9e944c3bce6c898fe9238e30adf5479b4d8
Filip Skaug <filis@stud.ntnu.no>
2024-10-03 12:31:58 +0200

    resize sidebar

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -16,36 +16,38 @@ const scrollTo = (section: string): void => {
 
 const Sidebar: React.FC<Props> = ({ active }) => {
   return (
-  <div className="sidebarWrapper">
-    <div className="buttonWrapper">
-      <button className={`navButton ${active == "home" ? "active" : ""}`} style={{backgroundColor: "#BBE9FF"}} onClick={() => scrollTo("home")}>
-        <HiHome />
-      </button>
-      <p>Start</p>
-    </div>
-    <div className="buttonWrapper">
-      <button className={`navButton ${active == "profile" ? "active" : ""}`} style={{backgroundColor: "#FDFD96"}} onClick={() => scrollTo("profile")}>
-        <HiUserCircle />
-      </button>
-      <p>Om Meg</p>
-    </div>
-    <div className="buttonWrapper">
-      <button className={`navButton ${active == "education" ? "active" : ""}`} style={{backgroundColor: "#e0d6ff"}} onClick={() => scrollTo("education")}>
-        <HiAcademicCap />
-      </button>
-      <p>Erfaring</p>
-    </div>
-    <div className="buttonWrapper">
-      <button className={`navButton ${active == "projects" ? "active" : ""}`} style={{backgroundColor: "#c2efaa"}} onClick={() => scrollTo("projects")}>
-        <HiFolder />
-      </button>
-      <p>Prosjekter</p>
-    </div>
-    <div className="buttonWrapper">
-      <button className={`navButton ${active == "contact" ? "active" : ""}`} style={{backgroundColor: "#FF8787"}} onClick={() => scrollTo("contact")}>
-        <HiMail />
-      </button>
-      <p>Kontakt</p>
+  <div className="sidebarContent">
+    <div className="sidebarWrapper">
+      <div className="buttonWrapper">
+        <button className={`navButton ${active == "home" ? "active" : ""}`} style={{backgroundColor: "#BBE9FF"}} onClick={() => scrollTo("home")}>
+          <HiHome />
+        </button>
+        <p>Start</p>
+      </div>
+      <div className="buttonWrapper">
+        <button className={`navButton ${active == "profile" ? "active" : ""}`} style={{backgroundColor: "#FDFD96"}} onClick={() => scrollTo("profile")}>
+          <HiUserCircle />
+        </button>
+        <p>Om Meg</p>
+      </div>
+      <div className="buttonWrapper">
+        <button className={`navButton ${active == "education" ? "active" : ""}`} style={{backgroundColor: "#e0d6ff"}} onClick={() => scrollTo("education")}>
+          <HiAcademicCap />
+        </button>
+        <p>Erfaring</p>
+      </div>
+      <div className="buttonWrapper">
+        <button className={`navButton ${active == "projects" ? "active" : ""}`} style={{backgroundColor: "#c2efaa"}} onClick={() => scrollTo("projects")}>
+          <HiFolder />
+        </button>
+        <p>Prosjekter</p>
+      </div>
+      <div className="buttonWrapper">
+        <button className={`navButton ${active == "contact" ? "active" : ""}`} style={{backgroundColor: "#FF8787"}} onClick={() => scrollTo("contact")}>
+          <HiMail />
+        </button>
+        <p>Kontakt</p>
+      </div>
     </div>
   </div>
   )
--- src/pages/sections/home.tsx
+++ src/pages/sections/home.tsx
@@ -3,7 +3,7 @@ import { TypeAnimation } from 'react-type-animation';
 
 function home() {
   return(
-  <div className="section home">
+  <div className="section home" id="home">
     <div>
       <p className="intro1">Hei,</p>
       <p className="intro2">Jeg heter Filip Skaug</p>
