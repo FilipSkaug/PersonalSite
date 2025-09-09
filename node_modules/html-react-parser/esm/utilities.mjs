a6127e8eb158a955ecae01cf94b006a17148678b
Filip Skaug <filis@stud.ntnu.no>
2025-09-09 15:52:49 +0200

    Remove node_modules/cache

--- node_modules/html-react-parser/esm/utilities.mjs
+++
@@ -1,8 +0,0 @@
-export {
-  canTextBeChildOfNode,
-  ELEMENTS_WITH_NO_TEXT_CHILDREN,
-  isCustomComponent,
-  PRESERVE_CUSTOM_ATTRIBUTES,
-  returnFirstArg,
-  setStyleProp,
-} from '../lib/utilities.js';
